d92a1d360f61b05ca382f09d74b3a6f6915f6511
Burim Palloshi <burim-palloshi@hotmail.com>
2023-06-18 00:13:59 +0200

    starting on mainContent

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,10 @@ import img1 from "../assets/logo.svg";
 const Header = () => {
   return (
     <div className="header">
-      <p>My balance</p>
-      <h1>$921.48</h1>
+      <div className="header-left">
+        <p>My balance</p>
+        <h2>$921.48</h2>
+      </div>
       <img alt="" src={img1}></img>
     </div>
   );
--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const MainContent = () => {
-  return <div className="main-content"></div>;
+  return (
+    <div className="main-content">
+      <h2>Spending - Last 7 days</h2>
+    </div>
+  );
 };
 
 export default MainContent;
